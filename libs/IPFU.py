7222494d128c13bf06711fd2d91837d6cd4ce665
kzoltan <zoltan.koszo@gmail.com>
2018-03-10 11:26:21 +0100

    pep8/function/method changes

--- libs/IPFU.py
+++ libs/IPFU.py
@@ -1,57 +1,72 @@
 #!/usr/bin/python
+""" IPFU helper function """
 
-import logging, time, os, sys, inspect, socket, nfqueue, ipcalc, struct
-import importlib, scandir
-#from netaddr import IPAddress
-import netaddr
-import netifaces
-sys.path.append("./mods")
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)	# prevent scapy warnings for ipv6
+import logging
+import inspect
+import netaddr, netifaces
 from scapy import all as scapy
-#from netaddr import IPAddress
 
+# prevent scapy warnings for ipv6
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 scapy.conf.verb = 0
 
-# Our helper logger class
-class IPFU:
-	def __pfu_out(self, msg, msgcolor):
-		color = {  "mod": '\033[90m', "msg": '\033[94m',
-				"err": '\033[91m', "end": '\033[0m' }
-		mod = []
-		for fr in reversed(inspect.stack()[2:-2]):
-			mod.append(fr[3])
-		print color["mod"] + '.'.join(mod) +' > '+ color[msgcolor] + str(msg) + color["end"]
+class IPFU(object):
+    """ Our helper logger class """
+    def __init__(self):
+        pass
+    def __pfu_out(self, msg, msgcolor):
+        """ used for printing """
+        color = {\
+        "mod": '\033[90m',\
+        "msg": '\033[94m',\
+        "err": '\033[91m',\
+        "end": '\033[0m'}
+        mod = []
+        for frame in reversed(inspect.stack()[2:-2]):
+            mod.append(frame[3])
+        print color["mod"] + '.'.join(mod) +' > ' +\
+        color[msgcolor] + str(msg) + color["end"]
 
-	def msg(self, msg):
-		self.__pfu_out(msg, "msg")
-	def err(self, msg):
-		self.__pfu_out(msg, "err")
+    def msg(self, msg):
+        """ wrapper for __pfu_out """
+        self.__pfu_out(msg, "msg")
+    def err(self, msg):
+        """ wrapper for __pfu_out """
+        self.__pfu_out(msg, "err")
 
-	def getMyIP(self, iface):
-		return netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
+    def getMyIP(self, iface):
+        """ return IP of iface parameter interface """
+        return netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
 
-	def getMyNet(self, iface):
-		ip = self.getMyIP(iface)
-		bits = str(netaddr.IPAddress(netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['netmask']).netmask_bits())
-		net = netaddr.IPNetwork(ip+'/'+bits)
-		ret = str(net.network) + '/' + bits
-		return ret
+    def getMyNet(self, iface):
+        """ return net? """
+        ip_address = self.getMyIP(iface)
+        bits = str(netaddr.IPAddress(\
+                netifaces.ifaddresses(iface)\
+                [netifaces.AF_INET][0]['netmask']).netmask_bits())
+        net = netaddr.IPNetwork(ip_address+'/'+bits)
+        ret = str(net.network) + '/' + bits
+        return ret
 
-	def getmacs(self, target):
-		ret = {'ip_mac': {}, 'mac_ip': {}}
-		self.msg('arping in progress')
-		ans,unans = scapy.arping(target)
-		self.msg('arping finished')
-		for a in ans:
-#			a[1].show()
-			mac = a[1][scapy.ARP].hwsrc
-			ip = a[1][scapy.ARP].psrc
-			if mac in ret['mac_ip']:
-				ret['mac_ip'][mac].append(ip)
-			else:
-				ret['mac_ip'][mac] = [ip]
-			ret['ip_mac'][ip] = mac
-		return ret
-
-
-
+    def getmacs(self, target):
+        """ get scapy arping for target and order it """
+        address_table = {'ip_mac': {}, 'mac_ip': {}}
+        self.msg('arping in progress')
+        answers, unans = scapy.arping(target)
+        self.msg('arping finished')
+        for answer in answers:
+            # a[1].show()
+            # get mac and ip from scapy output
+            mac_addr = answer[1][scapy.ARP].hwsrc
+            ip_addr = answer[1][scapy.ARP].psrc
+            # one MAC can have multiple IP addresses, so mac_ip
+            # will have mac - ip_array pairs
+            if mac_addr in address_table['mac_ip']:
+                address_table['mac_ip'][mac_addr].append(ip_addr)
+            else:
+                address_table['mac_ip'][mac_addr] = [ip_addr]
+            # one ip can be paired to only one MAC address
+            address_table['ip_mac'][ip_addr] = mac_addr
+        # {'ip_mac': {'192.168.110.1': 'e4:8d:8c:a0:e8:a9'},
+        #  'mac_ip': {'e4:8d:8c:a0:e8:a9': ['192.168.110.1']}}
+        return address_table
--- mods/arping.py
+++ mods/arping.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import logging
 from IPFU import *
+
+import logging
 from scapy import all as scapy
 from netaddr import IPAddress
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)	# prevent scapy warnings for ipv6
 scapy.conf.verb = 0
-
 
 # arping module
 class arping(IPFU):
@@ -15,27 +15,31 @@ class arping(IPFU):
 	"""
 	def __init__(self, params=None):
 		try:
-			self.req = params[0]
+			self.command = params[0]
 			self.target = params[1]
 		except:
 			print self.__doc__
 			if params is not None: exit(1)
 
 	def mac2ip(self):
-		for mac in self.tab['mac_ip']:
-			print mac, " ".join(self.tab['mac_ip'][mac])
+		for mac in self.address_table['mac_ip']:
+			print mac, " ".join(self.address_table['mac_ip'][mac])
 
 	def ip2mac(self):
-		for ip in self.tab['ip_mac']:
-			print ip, self.tab['ip_mac'][ip]
+		for ip in self.address_table['ip_mac']:
+			print ip, self.address_table['ip_mac'][ip]
 
 	def start(self):
-		self.tab = self.getmacs(self.target)
-		if self.req == 'ip2mac': self.mac2ip()
-		elif self.req == 'mac2ip': self.ip2mac()
-		elif self.req == 'all':
+		self.address_table = self.getmacs(self.target)
+		if self.command == 'ip2mac':
+			# TODO: sort
+			print '=== IP2MAC'
+			self.mac2ip()
+		elif self.command == 'mac2ip':
+			print '=== MAC2IP'
+			self.ip2mac()
+		elif self.command == 'all':
 			print '=== IP2MAC'
 			self.ip2mac()
 			print '=== MAC2IP'
 			self.mac2ip()
-
